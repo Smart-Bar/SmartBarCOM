c74f109110868ff741530ec497eb80d515cb7680
iMega34 <mega2207@outlook.com>
2024-10-16 23:51:52 -0600

    Implemented servo control

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -22,7 +22,8 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#include <stdlib.h>
+#include <string.h>
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -32,8 +33,30 @@
 
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
-#define RX_BUFFER_SIZE 64
-#define COMMNAND_BUFFER_SIZE 5
+// Length of the receive buffer
+#define RX_BUFFER_SIZE          16
+
+// Length of the circular buffer
+#define CIRC_BUFFER_SIZE        8
+
+// Length of the command parameters
+#define ARG_LENGTH              6
+
+// Length of the command vector
+#define CMD_VCTR_SIZE           5
+
+// Minimum duty cycle for the servo motor (2.5% or 0°)
+#define DUTY_CYCLE_LOWER_BOUND  2.5
+
+// Maximum duty cycle for the servo motor (12.5% or 180°)
+#define DUTY_CYCLE_UPPER_BOUND  12.5
+
+// Minimum pulse width for the servo motor
+#define MIN_PULSE_WIDTH         6.375
+
+// Maximum pulse width for the servo motor
+#define MAX_PULSE_WIDTH         31.875
+
 /* USER CODE END PD */
 
 /* Private macro -------------------------------------------------------------*/
@@ -42,6 +65,8 @@
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
+TIM_HandleTypeDef htim3;
+
 UART_HandleTypeDef huart2;
 UART_HandleTypeDef huart3;
 
@@ -59,17 +84,26 @@ const osThreadAttr_t LED2_attributes = {
   .stack_size = 128 * 4,
   .priority = (osPriority_t) osPriorityNormal,
 };
+/* Definitions for PWM1 */
+osThreadId_t PWM1Handle;
+const osThreadAttr_t PWM1_attributes = {
+  .name = "PWM1",
+  .stack_size = 128 * 4,
+  .priority = (osPriority_t) osPriorityNormal,
+};
 /* USER CODE BEGIN PV */
 
 // Buffer for storing received data from UART
-uint8_t rxBuffer[RX_BUFFER_SIZE];
-// Circular buffer for storing commands
-uint8_t commandBuffer[COMMNAND_BUFFER_SIZE][RX_BUFFER_SIZE];
+char rxBuffer[RX_BUFFER_SIZE];
+
+// Circular buffer for storing commands -> Stores up to 8 commands with 5 parameters each
+char cmdVctr[CIRC_BUFFER_SIZE][CMD_VCTR_SIZE][ARG_LENGTH];
 
 // Flags and indexes
 volatile uint8_t enterPressed = 0;
 volatile uint8_t cmdWriteIdx = 0;
 volatile uint8_t cmdReadIdx = 0;
+volatile uint8_t cmdArgIdx = 0;
 volatile uint8_t cmdCount = 0;
 
 // Stores the last received character
@@ -84,11 +118,13 @@ void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_USART2_UART_Init(void);
 static void MX_USART3_UART_Init(void);
+static void MX_TIM3_Init(void);
 void StartLED1(void *argument);
 void StartLED2(void *argument);
+void StartPWM1(void *argument);
 
 /* USER CODE BEGIN PFP */
-
+void Parse_Command(void);
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
@@ -127,8 +163,14 @@ int main(void)
   MX_GPIO_Init();
   MX_USART2_UART_Init();
   MX_USART3_UART_Init();
+  MX_TIM3_Init();
   /* USER CODE BEGIN 2 */
   HAL_UART_Receive_IT(&huart3, textChar, 1);
+  HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_2);
+
+  // Set the initial angle of the servo motor to 0°
+  uint8_t pulse = (uint8_t)((DUTY_CYCLE_LOWER_BOUND / 100.0) * 255);
+  __HAL_TIM_SET_COMPARE(&htim3, TIM_CHANNEL_2, pulse);
   /* USER CODE END 2 */
 
   /* Init scheduler */
@@ -156,6 +198,9 @@ int main(void)
 
   /* creation of LED2 */
   LED2Handle = osThreadNew(StartLED2, NULL, &LED2_attributes);
+
+  /* creation of PWM1 */
+  PWM1Handle = osThreadNew(StartPWM1, NULL, &PWM1_attributes);
 
   /* USER CODE BEGIN RTOS_THREADS */
   /* add threads, ... */
@@ -219,13 +264,64 @@ void SystemClock_Config(void)
   {
     Error_Handler();
   }
-  PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_USART2|RCC_PERIPHCLK_USART3;
+  PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_USART2|RCC_PERIPHCLK_USART3
+                              |RCC_PERIPHCLK_TIM34;
   PeriphClkInit.Usart2ClockSelection = RCC_USART2CLKSOURCE_PCLK1;
   PeriphClkInit.Usart3ClockSelection = RCC_USART3CLKSOURCE_PCLK1;
+  PeriphClkInit.Tim34ClockSelection = RCC_TIM34CLK_HCLK;
   if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK)
   {
     Error_Handler();
   }
+}
+
+/**
+  * @brief TIM3 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_TIM3_Init(void)
+{
+
+  /* USER CODE BEGIN TIM3_Init 0 */
+
+  /* USER CODE END TIM3_Init 0 */
+
+  TIM_MasterConfigTypeDef sMasterConfig = {0};
+  TIM_OC_InitTypeDef sConfigOC = {0};
+
+  /* USER CODE BEGIN TIM3_Init 1 */
+
+  /* USER CODE END TIM3_Init 1 */
+  htim3.Instance = TIM3;
+  htim3.Init.Prescaler = 5625 - 1;
+  htim3.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim3.Init.Period = 255;
+  htim3.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+  htim3.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+  if (HAL_TIM_PWM_Init(&htim3) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
+  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+  if (HAL_TIMEx_MasterConfigSynchronization(&htim3, &sMasterConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sConfigOC.OCMode = TIM_OCMODE_PWM1;
+  sConfigOC.Pulse = 0;
+  sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
+  sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
+  if (HAL_TIM_PWM_ConfigChannel(&htim3, &sConfigOC, TIM_CHANNEL_2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN TIM3_Init 2 */
+
+  /* USER CODE END TIM3_Init 2 */
+  HAL_TIM_MspPostInit(&htim3);
+
 }
 
 /**
@@ -351,14 +447,14 @@ void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart) {
   if (huart->Instance != USART3)
     return;
 
-  // Any character except backspace and enter is received
+  // Add the received character to the buffer. Ignore Enter and Backspace
   if (textChar[0] != 13 && textChar[0] != 8) {
     rxBuffer[rxIdx++] = textChar[0];
     // Echo the received character
     HAL_UART_Transmit(&huart2, textChar, 1, 100);
   }
 
-  // Backspace key pressed
+  // Handle backspace. Remove the last character from the buffer
   if (textChar[0] == 8 && rxIdx > 0) {
     rxIdx--;
     rxBuffer[rxIdx] = 0;
@@ -366,25 +462,80 @@ void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart) {
     HAL_UART_Transmit(&huart2, (uint8_t*)"\b \b", 3, 100);
   }
 
-  // Enter key pressed
+  // Handle Enter. Reset the buffer and handle the command
   if (textChar[0] == 13) {
-    // Send the received command to the command buffer if there is enough
-    // space in the buffer
-    if (cmdCount < COMMNAND_BUFFER_SIZE) {
-      strcpy(commandBuffer[cmdWriteIdx], rxBuffer);
-      cmdWriteIdx = (cmdWriteIdx + 1) % COMMNAND_BUFFER_SIZE;
-      cmdCount++;
-    }
-
     // Reset the buffer and the index
     rxIdx = 0;
-    memset(rxBuffer, 0, RX_BUFFER_SIZE);
+
+    // Send the received command to the command buffer if there is enough
+    // space in the buffe r
+    if (cmdCount < CMD_VCTR_SIZE) {
+      Parse_Command();
+    }
+
     // Echo a newline character
     HAL_UART_Transmit(&huart2, (uint8_t*)"\n\r", 2, 100);
+    memset(rxBuffer, 0, RX_BUFFER_SIZE);
   }
 
   // Restart the interrupt-driven reception of a single character
   HAL_UART_Receive_IT(&huart3, textChar, 1);
+}
+
+void Parse_Command(void) {
+  // Parse the command to get its parameters
+  char *input = (char*) rxBuffer;
+  char *charPointer = strtok(input, " ");
+  strcpy(cmdVctr[cmdWriteIdx][cmdArgIdx++], charPointer);
+
+  // Continue parsing the command until the end of the buffer
+  do {
+    charPointer = strtok(NULL, " ");
+    if (charPointer != NULL && cmdArgIdx < CMD_VCTR_SIZE) {
+      strcpy(cmdVctr[cmdWriteIdx][cmdArgIdx++], charPointer);
+    }
+  } while (charPointer != NULL && cmdArgIdx < CMD_VCTR_SIZE);
+
+  // Reset the command argument vector index, move to the next write index and increase the command count
+  cmdArgIdx = 0;
+  cmdWriteIdx = (cmdWriteIdx + 1) % CMD_VCTR_SIZE;
+  cmdCount++;
+
+  memset(rxBuffer, 0, sizeof(rxBuffer));
+}
+
+/**
+ * @brief Clamps a value between a lower and upper bound.
+ * 
+ * @param value Value to be clamped
+ * @param lowerBound Lower bound
+ * @param upperBound Upper bound
+ * 
+ * @retval Clamped value
+ */
+float clamp(float input, float lowerBound, float upperBound) {
+  return input < lowerBound
+    ? lowerBound
+    : input > upperBound
+      ? upperBound
+      : input;
+}
+
+/**
+ * @brief Maps a value from one range to another.
+ * 
+ * @param value Value to be mapped
+ * @param fromLower Lower bound of the input range
+ * @param fromUpper Upper bound of the input range
+ * @param toLower Lower bound of the output range
+ * @param toUpper Upper bound of the output range
+ * 
+ * @retval Mapped value
+ * 
+ * @ref https://docs.arduino.cc/language-reference/en/functions/math/map/
+ */
+float map(float value, float fromLower, float fromUpper, float toLower, float toUpper) {
+  return (value - fromLower) * (toUpper - toLower) / (fromUpper - fromLower) + toLower;
 }
 /* USER CODE END 4 */
 
@@ -403,14 +554,15 @@ void StartLED1(void *argument)
     if (cmdCount == 0)
       continue;
 
-    if (strcmp(commandBuffer[cmdReadIdx], "LED1") == 0) {
-      // Increase the read index and decrease the command count
-      cmdReadIdx = (cmdReadIdx + 1) % COMMNAND_BUFFER_SIZE;
-      cmdCount--;
+    if (strcmp(cmdVctr[cmdReadIdx][0], "LED1") == 0) {
       // Execute the command
       HAL_GPIO_WritePin(GPIOB, GPIO_PIN_8, GPIO_PIN_SET);
       osDelay(2000);
       HAL_GPIO_WritePin(GPIOB, GPIO_PIN_8, GPIO_PIN_RESET);
+
+      // Increase the read index and decrease the command count
+      cmdReadIdx = (cmdReadIdx + 1) % CMD_VCTR_SIZE;
+      cmdCount--;
     }
   }
 
@@ -433,17 +585,66 @@ void StartLED2(void *argument)
     if (cmdCount == 0)
       continue;
 
-    if (strcmp(commandBuffer[cmdReadIdx], "LED2") == 0) {
-      // Increase the read index and decrease the command count
-      cmdReadIdx = (cmdReadIdx + 1) % COMMNAND_BUFFER_SIZE;
-      cmdCount--;
+    if (strcmp(cmdVctr[cmdReadIdx][0], "LED2") == 0) {
       // Execute the command
       HAL_GPIO_WritePin(GPIOB, GPIO_PIN_9, GPIO_PIN_SET);
       osDelay(3000);
       HAL_GPIO_WritePin(GPIOB, GPIO_PIN_9, GPIO_PIN_RESET);
+
+      // Increase the read index and decrease the command count
+      cmdReadIdx = (cmdReadIdx + 1) % CMD_VCTR_SIZE;
+      cmdCount--;
     }
   }
+
+  osThreadTerminate(NULL);
   /* USER CODE END StartLED2 */
+}
+
+/* USER CODE BEGIN Header_StartPWM1 */
+/**
+* @brief Function implementing the PWM1 thread.
+* @param argument: Not used
+* @retval None
+*/
+/* USER CODE END Header_StartPWM1 */
+void StartPWM1(void *argument)
+{
+  /* USER CODE BEGIN StartPWM1 */
+  /* Infinite loop */
+  while (1) {
+    // Ignore id there is no command in the buffer
+    if (cmdCount == 0)
+      continue;
+
+    if (strcmp(cmdVctr[cmdReadIdx][0], "PWM1") ==  0) {
+      // Execute the command
+      char param1[ARG_LENGTH];
+      strcpy(param1, cmdVctr[cmdReadIdx][1]);
+      float dutyCycle;
+
+      // Check if the input is a percentage or a duty cycle value
+      if (strcmp(param1, "-p") == 0) {
+        dutyCycle = atof(cmdVctr[cmdReadIdx][2]);
+        dutyCycle = map(dutyCycle, 0, 100, DUTY_CYCLE_LOWER_BOUND, DUTY_CYCLE_UPPER_BOUND);
+      } else {
+        dutyCycle = atof(param1);
+        dutyCycle = clamp(dutyCycle, DUTY_CYCLE_LOWER_BOUND, DUTY_CYCLE_UPPER_BOUND);
+      }
+
+      // Calculate the pulse width and set the duty cycle of the signal
+      uint8_t pulse = (uint8_t)((dutyCycle / 100.0) * 255);
+      TIM3->CCR2 = pulse;
+      // __HAL_TIM_SET_COMPARE(&htim3, TIM_CHANNEL_2, pulse);
+
+      // Increase the read index and decrease the command count
+      cmdReadIdx = (cmdReadIdx + 1) % CMD_VCTR_SIZE;
+      cmdCount--;
+    }
+  }
+
+  osThreadTerminate(NULL);
+  /* USER CODE END StartPWM1 */
 }
 
 /**
